f5f00ca2db90cb35f1efd049be12e4966ec7e600
関口慶一 <sekiguchi@bpe.agr.hokudai.ac.jp>
2020-05-03 16:53:08 +0900

    プロジェクト ファイルを追加します。

---
+++ オブジェクト指向/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace オブジェクト指向
+{
+    class Vehicle
+    {
+        public int Passengers;
+        public int FuelCap;
+        public int Mpg;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle minivan = new Vehicle();
+            int range;
+            minivan.Passengers = 7;
+            minivan.FuelCap = 16;
+            minivan.Mpg = 21;
+
+            range = minivan.FuelCap * minivan.Mpg;
+            Console.WriteLine(range);
+        }
+    }
+}
